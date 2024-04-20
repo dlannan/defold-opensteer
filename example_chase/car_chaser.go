548683c9f525029649d7832a5c31e286472d90c7
David Lannan <david.lannan@caytrix.com>
2024-04-21 00:53:25 +0930

    More improvements. Chaser script working well.

---
+++ example_chase/car_chaser.go
@@ -0,0 +1,28 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"cars_police01\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/cars.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.1
+    z: 0.0
+  }
+  rotation {
+    x: 0.70710677
+    y: 0.0
+    z: 0.0
+    w: 0.70710677
+  }
+  scale {
+    x: 0.1
+    y: 0.1
+    z: 0.1
+  }
+}
---
+++ example_chase/car_player.go
@@ -0,0 +1,23 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"cars_player01\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/cars.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
